297990026a4599a4b4a0069a743bfbaecc819ade
AndresHabchi <andres.hhza@gmail.com>
2023-01-27 20:44:47 +0200

    hobby added

--- Profile.java
+++ Profile.java
@@ -3,7 +3,7 @@ public class Profile {
 		System.out.println("My name is Andres Habchi");
 		System.out.println("i am majoring in electrical engineering");
 		System.out.println("i am in my 2nd year of study");
-		
+		System.out.println("i play rugby, love bodybuilding and constructing electric circuits")
 		
 }
 }
