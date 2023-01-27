81c8eb207a96b76a7759fdf9b0316f7e2ac852bd
AndresHabchi <andres.hhza@gmail.com>
2023-01-27 20:42:39 +0200

    initial info provided

--- Profile.java
+++ Profile.java
@@ -3,5 +3,7 @@ public class Profile {
 		System.out.println("My name is Andres Habchi");
 		System.out.println("i am majoring in electrical engineering");
 		System.out.println("i am in my 2nd year of study");
+		
+		
 }
 }
