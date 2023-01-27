7bddb06daec30db1e2a4aa246b98dc5b78b8b432
AndresHabchi <andres.hhza@gmail.com>
2023-01-27 20:28:24 +0200

    the problems have been solved!

---
+++ Problems.java
@@ -0,0 +1,8 @@
+Public class Problems {
+public static void (String[]args);{
+	System.out.print("Hi, can we check this?");
+	System.out.println("It has a lot of problems");
+	System.out.prints("Yeah, I know!!!");
+/This is a comment
+}
+}
